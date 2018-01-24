c0eecbccd0433eb1f2f9ff3d25fd05fa3c480dc5
Paul Sztajer <paul@seethroughstudios.com>
2018-01-24 16:13:03 +0800

    objectpool recycle playmaker action

---
+++ ObjectPool/RecycleAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Object Pool")]
+    [Tooltip("Recycles a GameObject")]
+    public class RecycleAction : ActionDo
+    {
+        [RequiredField]
+        [Tooltip("The Gameobject to Recycle")]
+        public FsmOwnerDefault gameObject;
+
+        protected override void DoAction()
+        {
+            Fsm.GetOwnerDefaultTarget(gameObject).Recycle();
+        }
+    }
+}
